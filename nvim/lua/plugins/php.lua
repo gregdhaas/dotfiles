37d4a2177bd029c17779f7be8af2a1724d3ce9bb
Gregory Haas <ghaas@ghaas-macbook.tailce794.ts.net>
2025-03-05 17:33:46 -0500

    fix issue with blade parser config

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -65,7 +65,7 @@ return {
 
 			require("nvim-treesitter.configs").setup(opts)
 			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			parser_config.blade = {
+			parser_config["blade"] = {
 				install_info = {
 					url = "https://github.com/EmranMR/tree-sitter-blade",
 					files = { "src/parser.c" },
